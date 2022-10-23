d64877b589b7ff88b6979e7ab4539db9bba337bd
ameinias09 <116481799+ameinias09@users.noreply.github.com>
2022-10-23 23:19:11 +0530

    Profile backend done

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ app.use(cors());
 app.use(express.json());
 const mongoose = require('mongoose')
 const { signup } = require('./models/signup')
-
+const { basicinfo } = require('./models/basicinfo')
+const { profileinfo } = require('./models/profileinfo')
 mongoose.connect('mongodb+srv://SDJava:SDJava09@projectrp.6herpzj.mongodb.net/?retryWrites=true&w=majority').then(() => {
     console.log("Connected")
 }).catch((err) => {
@@ -16,6 +17,30 @@ mongoose.connect('mongodb+srv://SDJava:SDJava09@projectrp.6herpzj.mongodb.net/?r
 app.post('/SignUp', async(req, res) => {
     console.log("inside")
     const data_add = new signup(req.body)
+    try {
+        await data_add.save()
+        res.json(data_add)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error);
+    }
+})
+
+app.post('/BasicInfo', async(req, res) => {
+    console.log("inside")
+    const data_add = new basicinfo(req.body)
+    try {
+        await data_add.save()
+        res.json(data_add)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error);
+    }
+})
+
+app.post('/ProfileInfo', async(req, res) => {
+    console.log("inside")
+    const data_add = new profileinfo(req.body)
     try {
         await data_add.save()
         res.json(data_add)
