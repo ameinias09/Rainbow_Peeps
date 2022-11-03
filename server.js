74143f045c7167439e10f03a41d71efca6c8149c
ameinias09 <116481799+ameinias09@users.noreply.github.com>
2022-11-03 15:50:42 +0530

    disabled cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require('cors');
 const app = express()
-app.use(cors());
+    // app.use(cors());
 const bcrypt = require('bcrypt')
 app.use(express.json());
 const mongoose = require('mongoose')
