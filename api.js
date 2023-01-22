f5b3d00354ff871f1b30d82a919cf62605e02f75
ameinias09 <116481799+ameinias09@users.noreply.github.com>
2023-01-22 22:25:26 +0530

    env

--- api.js
+++ api.js
@@ -12,10 +12,10 @@ const { findingfriend } = require('./models/findingfriend')
 const { lgbtq } = require('./models/lgbtq')
 const { memberintroduction } = require('./models/memberintroduction')
 const { mentalhealth } = require('./models/mentalhealth')
-
+require("dotenv").config();
 
 app.use(cookieParser())
-mongoose.connect('mongodb+srv://RainbowPeeps:RainbowPeeps57@rainbowpeeps.gvhsno1.mongodb.net/test').then(() => {
+mongoose.connect(process.env.DB_KEY).then(() => {
     console.log("Connected")
 }).catch((err) => {
     console.log(err)
