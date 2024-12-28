be629c7f0261af5e88b5b42f89c75ad436fa7dc3
ameinias09 <116481799+ameinias09@users.noreply.github.com>
2024-12-28 05:52:45 +0530

    Dummy checkin

--- Profile/BasicInfo.js
+++ Profile/BasicInfo.js
@@ -15,27 +15,6 @@ btn.addEventListener('click', e => {
     document.cookie = "rorientation" + "=" + rorientation.value + "" + "; path=/"
     document.cookie = "pronouns" + "=" + pronouns.value + "" + "; path=/"
     document.cookie = "sorientation" + "=" + sorientation.value + "" + "; path=/"
-        // fetch('https://rainbowpeeps.onrender.com/BasicInfo', {
-        //         method: 'POST', // or 'PUT'
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             'Content-Type': 'application/json',
-        //             'Content-Type': 'application/json',
-        //             'Content-Type': 'application/json',
-        //             'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify({
-        //             dob: dob.value,
-        //             gender: gender.value,
-        //             pronouns: pronouns.value,
-        //             rorientation: rorientation.value,
-        //             sorientation: sorientation.value,
-        //         }),
-        //     })
-        //     .then(res => {
-        //         return res.json()
-        //     })
-        //     .then(data => console.log(data))
 
     window.location.replace("ProfileInfo.html");
 })
